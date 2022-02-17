9f37c1b45cd04d5892fbe870e409c15d77631064
Ahm54 <52510629+Ahm54@users.noreply.github.com>
2022-02-17 15:09:26 +0300

    kayıt ekleme silme görüntüleme
    C# ile yazılmış kayıt ekleme silme görüntüleme otomasyon uygulaması

---
+++ kayıteklemesilmegörüntüleme/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace kayıteklemesilmegörüntüleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Sıra");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("KitapAdı");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("Yazar");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("SayfaSayısı");
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4});
+            this.listView1.Location = new System.Drawing.Point(0, 45);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(642, 263);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.BlueViolet;
+            this.button1.Location = new System.Drawing.Point(665, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Görüntüle";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.BackgroundImage = global::kayıteklemesilmegörüntüleme.Properties.Resources.kutukhaneicinkullanılacakresim;
+            this.ClientSize = new System.Drawing.Size(854, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ kayıteklemesilmegörüntüleme/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace kayıteklemesilmegörüntüleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection Baglan = new SqlConnection("Data Source=LAPTOP\\-9IQ5NO3T\\Initial Catalog=Kutukhane;Integrated Security=True");
+
+        private void verilerigoruntule()
+        {
+            Baglan.Open();
+            SqlCommand komut = new SqlCommand("Select *from kitaplar", Baglan);
+            SqlDataReader oku = komut.ExecuteReader();
+
+            while (oku.Read()) {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["id"].ToString();
+                ekle.SubItems.Add(oku["kitapad"].ToString());
+                ekle.SubItems.Add(oku["yazar"].ToString());
+                ekle.SubItems.Add(oku["yayinevi"].ToString());
+                ekle.SubItems.Add(oku["sayfasayisi"].ToString());
+
+                
+                listView1.Items.Add(ekle);
+
+
+                    
+            }
+            Baglan.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            verilerigoruntule();
+        }
+    }
+}
